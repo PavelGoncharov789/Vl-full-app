2e8341fc3ed6a9e3b907513cde366f8779b38929
Pavel Goncharov <pavel.Agonchar@yandex.ru>
2022-04-12 16:15:56 +0300

    aded link list and logicAdded logic for displaying a list of links, fixed redirect when creating a new link

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,23 @@
 import "materialize-css";
+import { Loader } from "./components/Loader";
 import { Navbar } from "./components/Navbar";
 import { AuthContext } from "./context/AuthContext";
 import { useAuth } from "./hooks/auth.hook";
 import { useRoutes } from "./routes";
 
 function App() {
-  const { token, login, logout, userId } = useAuth();
+  const { token, login, logout, userId, ready } = useAuth();
   const isAuthenticated = !!token;
   const routes = useRoutes(isAuthenticated);
 
+  if (!ready) {
+    return <Loader />;
+  }
   return (
     <AuthContext.Provider
       value={{ token, login, logout, userId, isAuthenticated }}
-    >{isAuthenticated && <Navbar/>}
+    >
+      {isAuthenticated && <Navbar />}
       {routes}
     </AuthContext.Provider>
   );
--- routes/link.rout.js
+++ routes/link.rout.js
@@ -45,13 +45,13 @@ router.get("/", auth, async (req, res) => {
   }
 });
 
-router.get("/:id", auth, async (req, res) => {
+router.get('/:id', auth, async (req, res) => {
   try {
-    const link = await Link.findById(req.params.id);
-    res.json(link);
+    const link = await Link.findById(req.params.id)
+    res.json(link)
   } catch (e) {
-    res.status(500).json({ message: e.message });
+    res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова' })
   }
-});
+})
 
 module.exports = router;
